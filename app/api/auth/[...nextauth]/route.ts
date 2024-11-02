ecc1911fbc3aa18fee617c123d112356fde366f6
sai karthik <84711965+saikarthik04@users.noreply.github.com>
2024-11-02 18:57:49 +0530

    auth configuration fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: AuthOptions = { 
+const authOptions: AuthOptions = { 
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
